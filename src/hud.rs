6a146fd3e5eca23b5e330aff7a4a7ac7d6e206b9
Matt McDonald <metzen@gmail.com>
2024-11-30 11:40:44 -0500

    Add some more hud stuff

--- src/hud.rs
+++ src/hud.rs
@@ -16,6 +16,7 @@ impl Plugin for HudPlugin {
                 update_velocity,
                 update_acceleration,
                 update_altitude,
+                update_hud_subject,
             ),
         );
     }
@@ -35,6 +36,9 @@ struct AccelerationText;
 
 #[derive(Component)]
 struct AltitudeText;
+
+#[derive(Component)]
+pub struct HudSubject;
 
 fn setup_hud(mut commands: Commands, asset_server: Res<AssetServer>) {
     let font = asset_server.load("fonts/Roboto-Medium.ttf");
@@ -108,51 +112,79 @@ fn update_fps(mut query: Query<&mut Text, With<TimeWarpText>>, time_warp: Res<Ti
     text.sections[1].value = format!("{:.2}", time_warp.value);
 }
 
-fn update_throttle(mut query: Query<&mut Text, With<ThrottleText>>, vessel_query: Query<&Vessel>) {
-    let vessel = vessel_query.single();
-    query.single_mut().sections[1].value = format!("{:.2}", vessel.throttle);
+fn update_throttle(mut query: Query<&mut Text, With<ThrottleText>>, vessel_query: Query<&Vessel, With<HudSubject>>) {
+    if let Some(vessel) = vessel_query.iter().next() {
+        query.single_mut().sections[1].value = format!("{:.2}", vessel.throttle);
+    }
 }
 
 fn update_velocity(
     mut query: Query<&mut Text, With<VelocityText>>,
-    vessel_rigidbody_query: Query<&RigidBody, With<Vessel>>,
+    vessel_rigidbody_query: Query<&RigidBody, With<HudSubject>>,
 ) {
-    let rigidbody = vessel_rigidbody_query.single();
-    query.single_mut().sections[1].value = format!("{:.2}", rigidbody.velocity);
+    if let Some(rigidbody) = vessel_rigidbody_query.iter().next() {
+        query.single_mut().sections[1].value = format!("{:.2}", rigidbody.velocity);
+    }
 }
 
 fn update_acceleration(
     mut query: Query<&mut Text, With<AccelerationText>>,
-    vessel_rigidbody_query: Query<&RigidBody, With<Vessel>>,
+    vessel_rigidbody_query: Query<&RigidBody, With<HudSubject>>,
 ) {
-    let rigidbody = vessel_rigidbody_query.single();
-    query.single_mut().sections[1].value = format!("{:.2}", rigidbody.acceleration);
+    if let Some(rigidbody) = vessel_rigidbody_query.iter().next() {
+        query.single_mut().sections[1].value = format!("{:.2}", rigidbody.acceleration);
+    }
 }
 
 fn update_altitude(
     mut query: Query<&mut Text, With<AltitudeText>>,
-    vessel_rigidbody_query: Query<
-        (&Transform, &RigidBody),
-        With<Vessel>,
-    >,
+    vessel_rigidbody_query: Query<(&Transform, &RigidBody), With<HudSubject>>,
     primary_transform_query: Query<(&Transform, &GridCell<i32>)>,
 ) {
-    let (vessel_transform, vessel_rigidbody) =
-        vessel_rigidbody_query.single();
-    if vessel_rigidbody.primary.is_some() {
-        let Ok((primary_transform, primary_grid_cell)) =
-            primary_transform_query.get(vessel_rigidbody.primary.unwrap())
-        else {
-            return;
-        };
-        // info!("primary: {:?}", primary_transform.translation);
-        // info!("vessel: {:?}", vessel_transform.translation);
-        // TODO: This needs to account for BigSpace grid_cell difference.
-        // let cell_diff = grid_cell - primary_grid_cell;
-        // let cell_distance = IVec3::new(cell_diff.x, cell_diff.y, cell_diff.z).distance(IVec3::ZERO);
-        let distance = primary_transform
-            .translation
-            .distance(vessel_transform.translation);
-        query.single_mut().sections[1].value = format!("{:.2}", distance);
+    if let Some((vessel_transform, vessel_rigidbody)) = vessel_rigidbody_query.iter().next() {
+        if vessel_rigidbody.primary.is_some() {
+            let Ok((primary_transform, primary_grid_cell)) =
+                primary_transform_query.get(vessel_rigidbody.primary.unwrap())
+            else {
+                return;
+            };
+            // info!("primary: {:?}", primary_transform.translation);
+            // info!("vessel: {:?}", vessel_transform.translation);
+            // TODO: This needs to account for BigSpace grid_cell difference.
+            // let cell_diff = grid_cell - primary_grid_cell;
+            // let cell_distance = IVec3::new(cell_diff.x, cell_diff.y, cell_diff.z).distance(IVec3::ZERO);
+            let distance = primary_transform
+                .translation
+                .distance(vessel_transform.translation);
+            query.single_mut().sections[1].value = format!("{:.2}", distance);
+        }
+    }
+}
+
+pub fn update_hud_subject(
+    mut commands: Commands,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut vessels_query: Query<(Entity, &mut Vessel, Option<&HudSubject>), With<Vessel>>,
+    subject_query: Query<Entity, With<HudSubject>>,
+) {
+    if keyboard_input.just_pressed(KeyCode::BracketLeft) {
+        info!("backet left");
+        for (entity, mut vessel, hud_subject) in vessels_query.iter_mut() {
+
+            info!("hud subj vessel");
+            if hud_subject.is_some() {
+                info!("vessel is subject");
+                commands.entity(entity).remove::<HudSubject>();
+                vessel.controlled = false;
+            } else {
+                info!("vessel is not subject");
+                commands.entity(entity).insert(HudSubject);
+                vessel.controlled = true;
+            }
+        }
+
+    }
+    if keyboard_input.just_pressed(KeyCode::BracketRight) {
+        
     }
 }
