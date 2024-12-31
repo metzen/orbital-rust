b0ba2ed0df81c9a5a2fc902021dd87e8731748ff
Matt McDonald <metzen@gmail.com>
2024-12-31 17:39:40 -0500

    Planet color in enum

--- src/scene.rs
+++ src/scene.rs
@@ -13,48 +13,59 @@ use big_space::{BigSpaceCommands, ReferenceFrame};
 pub struct Planet {
     pub mass: f32,
     pub radius: f32,
+    pub color: Color,
 }
 
 impl Planet {
     pub const SUN: Self = Self {
         mass: 1.9891e30,
         radius: 695.7e6,
+        color: Color::srgb(3.0, 3.0, 3.0),
     };
     pub const MERCURY: Self = Self {
         mass: 3.303e23,
         radius: 2.4397e6,
+        color: Color::Srgba(GRAY),
     };
     pub const VENUS: Self = Self {
         mass: 4.869e24,
         radius: 6.0518e6,
+        color: Color::srgb(0.75, 0.62, 0.43),
     };
     pub const EARTH: Self = Self {
         mass: 5.976e24,
         radius: 6.37814e6,
+        color: Color::srgb(0.06, 0.57, 0.98),
     };
     pub const MOON: Self = Self {
         mass: 7.3e22,
         radius: 1.74e6,
+        color: Color::WHITE,
     };
     pub const MARS: Self = Self {
         mass: 6.421e23,
         radius: 3.3972e6,
+        color: Color::Srgba(RED),
     };
     pub const JUPITER: Self = Self {
         mass: 1.9e27,
         radius: 71.492e6,
+        color: Color::Srgba(MAGENTA),
     };
     pub const SATURN: Self = Self {
         mass: 5.688e26,
         radius: 60.268e6,
+        color: Color::srgb(0.83, 0.74, 0.62),
     };
     pub const URANUS: Self = Self {
         mass: 8.686e25,
         radius: 25.559e6,
+        color: Color::Srgba(LIGHT_BLUE),
     };
     pub const NEPTUNE: Self = Self {
         mass: 1.024e26,
         radius: 24.746e6,
+        color: Color::Srgba(DARK_BLUE),
     };
 }
 
@@ -81,7 +92,7 @@ pub fn setup_scene(
             Name::new("Sun"),
             Mesh2d(meshes.add(Mesh::from(Circle::new(Planet::SUN.radius)))),
             Transform::from_xyz(0.0, 0.0, 1.0),
-            MeshMaterial2d(materials.add(ColorMaterial::from(Color::srgb(3.0, 3.0, 3.0)))),
+            MeshMaterial2d(materials.add(ColorMaterial::from(Planet::SUN.color))),
             RigidBody {
                 mass: Planet::SUN.mass,
                 velocity: Vec3::ZERO,
@@ -94,7 +105,7 @@ pub fn setup_scene(
             Name::new("Mercury"),
             Mesh2d(meshes.add(Mesh::from(Circle::new(Planet::MERCURY.radius)))),
             Transform::from_xyz(0.0, 46e9, 0.0),
-            MeshMaterial2d(materials.add(ColorMaterial::from(Color::from(GRAY)))),
+            MeshMaterial2d(materials.add(ColorMaterial::from(Planet::MERCURY.color))),
             RigidBody {
                 mass: Planet::MERCURY.mass,
                 velocity: Vec3 {
@@ -112,7 +123,7 @@ pub fn setup_scene(
             Name::new("Venus"),
             Mesh2d(meshes.add(Mesh::from(Circle::new(Planet::VENUS.radius)))),
             Transform::from_xyz(0.0, -108.2e9, 0.0),
-            MeshMaterial2d(materials.add(ColorMaterial::from(Color::from(YELLOW)))),
+            MeshMaterial2d(materials.add(ColorMaterial::from(Planet::VENUS.color))),
             RigidBody {
                 mass: Planet::VENUS.mass,
                 velocity: Vec3 {
@@ -133,7 +144,7 @@ pub fn setup_scene(
                 2000,
             )))),
             Transform::from_xyz(147.10e9, 0.0, 0.0),
-            MeshMaterial2d(materials.add(ColorMaterial::from(Color::srgb_u8(17, 145, 250)))),
+            MeshMaterial2d(materials.add(ColorMaterial::from(Planet::EARTH.color))),
             RigidBody {
                 velocity: Vec3 {
                     x: 0.0,
@@ -198,7 +209,7 @@ pub fn setup_scene(
             Name::new("Moon"),
             Mesh2d(meshes.add(Mesh::from(Circle::new(Planet::MOON.radius)))),
             Transform::from_xyz(147.10e9 + 385e6, 0.0, 0.0),
-            MeshMaterial2d(materials.add(ColorMaterial::from(Color::WHITE))),
+            MeshMaterial2d(materials.add(ColorMaterial::from(Planet::MOON.color))),
             RigidBody {
                 mass: Planet::MOON.mass,
                 velocity: Vec3 {
@@ -216,7 +227,7 @@ pub fn setup_scene(
             Name::new("Mars"),
             Mesh2d(meshes.add(Mesh::from(Circle::new(Planet::MARS.radius)))),
             Transform::from_xyz(206.7e9, 0.0, 0.0),
-            MeshMaterial2d(materials.add(ColorMaterial::from(Color::from(RED)))),
+            MeshMaterial2d(materials.add(ColorMaterial::from(Planet::MARS.color))),
             RigidBody {
                 mass: Planet::MARS.mass,
                 velocity: Vec3 {
@@ -234,7 +245,7 @@ pub fn setup_scene(
             Name::new("Jupiter"),
             Mesh2d(meshes.add(Mesh::from(Circle::new(Planet::JUPITER.radius)))),
             Transform::from_xyz(740.595e9, 0.0, 0.0),
-            MeshMaterial2d(materials.add(ColorMaterial::from(Color::from(MAGENTA)))),
+            MeshMaterial2d(materials.add(ColorMaterial::from(Planet::JUPITER.color))),
             RigidBody {
                 mass: Planet::JUPITER.mass,
                 velocity: Vec3 {
@@ -252,7 +263,7 @@ pub fn setup_scene(
             Name::new("Saturn"),
             Mesh2d(meshes.add(Mesh::from(Circle::new(Planet::SATURN.radius)))),
             Transform::from_xyz(1352.55e9, 0.0, 0.0),
-            MeshMaterial2d(materials.add(ColorMaterial::from(Color::from(YELLOW)))),
+            MeshMaterial2d(materials.add(ColorMaterial::from(Planet::SATURN.color))),
             RigidBody {
                 mass: Planet::MARS.mass,
                 velocity: Vec3 {
@@ -270,7 +281,7 @@ pub fn setup_scene(
             Name::new("Uranus"),
             Mesh2d(meshes.add(Mesh::from(Circle::new(Planet::URANUS.radius)))),
             Transform::from_xyz(2735.56e9, 0.0, 0.0),
-            MeshMaterial2d(materials.add(ColorMaterial::from(Color::from(LIGHT_BLUE)))),
+            MeshMaterial2d(materials.add(ColorMaterial::from(Planet::URANUS.color))),
             RigidBody {
                 mass: Planet::MARS.mass,
                 velocity: Vec3 {
@@ -288,7 +299,7 @@ pub fn setup_scene(
             Name::new("Neptune"),
             Mesh2d(meshes.add(Mesh::from(Circle::new(Planet::NEPTUNE.radius)))),
             Transform::from_xyz(4471.050e9, 0.0, 0.0),
-            MeshMaterial2d(materials.add(ColorMaterial::from(Color::from(DARK_BLUE)))),
+            MeshMaterial2d(materials.add(ColorMaterial::from(Planet::NEPTUNE.color))),
             RigidBody {
                 mass: Planet::MARS.mass,
                 velocity: Vec3 {
