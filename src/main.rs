881c4a368e5eca61cd8fa92680983bb48f282cee
Matt McDonald <metzen@gmail.com>
2025-09-25 23:21:01 -0400

    Change default resolution to 720p 16:10

--- src/main.rs
+++ src/main.rs
@@ -72,7 +72,7 @@ fn main() {
                     primary_window: Some(Window {
                         title: String::from("Orbital"),
                         present_mode: bevy::window::PresentMode::AutoNoVsync,
-                        resolution: WindowResolution::new(1024.0, 768.0)
+                        resolution: WindowResolution::new(1152.0, 720.0)
                             .with_scale_factor_override(1.0),
                         ..default()
                     }),
