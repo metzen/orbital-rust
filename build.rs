cca64b9521b91e9aa38cf6e9e60b481bef71e58b
Matt McDonald <metzen@gmail.com>
2026-04-05 23:50:53 -0400

    Inline `target` variable

--- build.rs
+++ build.rs
@@ -1,8 +1,7 @@
 extern crate embed_resource;
 
 fn main() {
-    let target = std::env::var("TARGET").unwrap();
-    if target.contains("windows") {
+    if std::env::var("TARGET").unwrap().contains("windows") {
         embed_resource::compile("icon.rc", embed_resource::NONE)
             .manifest_optional()
             .unwrap();
