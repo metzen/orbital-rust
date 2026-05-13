5db97432e3af865995313e6745b7f8a931b21e54
Matt McDonald <metzen@gmail.com>
2026-05-12 21:36:13 -0400

    Invert LEFT_Y stick for rotate/pitch control

--- src/vessel.rs
+++ src/vessel.rs
@@ -92,7 +92,9 @@ impl VesselAction {
         InputMap::default()
             .with_axis(
                 Self::Rotate,
-                GamepadControlAxis::LEFT_Y.with_deadzone_symmetric(0.3),
+                GamepadControlAxis::LEFT_Y
+                    .with_deadzone_symmetric(0.3)
+                    .inverted(),
             )
             .with_axis(Self::Rotate, VirtualAxis::ad())
             .with_axis(Self::Rotate, VirtualAxis::new(KeyCode::KeyW, KeyCode::KeyS))
